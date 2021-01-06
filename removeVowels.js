ae254e1584c5c772b6fbc099c6f8f0d5d7fdfffb
jwnukoski <jwnukoski@protonmail.com>
2021-01-06 01:52:35 -0500

    removeVowels

---
+++ removeVowels.js
@@ -0,0 +1,9 @@
+// Prompt URL: https://leetcode.com/problems/remove-vowels-from-a-string/
+/**
+ * @param {string} s
+ * @return {string}
+ */
+// eslint-disable-next-line no-unused-vars
+const removeVowels = function (s) {
+  return s.replace(/a|e|i|o|u/g, '')
+}
