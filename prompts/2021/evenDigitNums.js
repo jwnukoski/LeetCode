f421c0bcce1b67081fb6d58a616a6cae8f34a8cc
jwnukoski <jwnukoski@protonmail.com>
2021-01-16 14:48:19 -0500

    evenDigitNums

---
+++ prompts/2021/evenDigitNums.js
@@ -0,0 +1,18 @@
+// Prompt URL: https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+
+/* eslint-disable no-unused-vars */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findNumbers = function (nums) {
+  let evenDigitCount = 0
+
+  nums.forEach(num => {
+    if (String(num).length % 2 === 0) {
+      evenDigitCount++
+    }
+  })
+
+  return evenDigitCount
+}
