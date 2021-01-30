7666378e3c77b2fb217a32aed769737d164a7f35
jwnukoski <jwnukoski@protonmail.com>
2021-01-30 16:26:02 -0500

    productOfArrayExceptSelf

---
+++ prompts/2021/productOfArrayExceptSelf.js
@@ -0,0 +1,36 @@
+// Prompt URL: https://leetcode.com/problems/product-of-array-except-self/
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const productExceptSelf = (nums) => {
+  const products = []
+
+  const left = [1]
+  for (let i = 1; i < nums.length; i++) {
+    left.push(nums[i - 1] * left[i - 1])
+  }
+
+  const right = []
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (i === nums.length - 1) {
+      right[i] = 1
+      continue
+    }
+
+    right[i] = right[i + 1] * nums[i + 1]
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    products[i] = left[i] * right[i]
+  }
+
+  return products
+}
+
+// Input:  [1,2,3,4]
+// Output: [24,12,8,6]
+
+// Runtime: 120 ms, faster than 70.21% of JavaScript online submissions for Product of Array Except Self.
+// Memory Usage: 49.9 MB, less than 46.92% of JavaScript online submissions for Product of Array Except Self.
