472476761120e57b260014ca8014c055f486afeb
jwnukoski <jwnukoski@protonmail.com>
2021-04-01 15:02:15 -0400

    depthFirstSearch

---
+++ prompts/2021/depthFirstSearch.js
@@ -0,0 +1,36 @@
+// Do not edit the class below except
+// for the depthFirstSearch method.
+// Feel free to add new properties
+// and methods to the class.
+class Node {
+    constructor(name) {
+      this.name = name;
+      this.children = [];
+    }
+  
+    addChild(name) {
+      this.children.push(new Node(name));
+      return this;
+    }
+  
+    depthFirstSearch(array) {
+          const traverse = (node) => {
+              array.push(node.name)
+  
+              if (this.children.length === 0) {
+                  return
+              } else {
+                  for (const child of node.children) {
+                      traverse(child)	
+                  }
+              }	
+          }
+          
+          traverse(this)
+          return array
+    }
+  }
+  
+  // Do not edit the line below.
+  exports.Node = Node;
+  
